2dd98f8bfa7937f3e91451c74682e78def965d9f
Yeli <yelizaveasiddik@gmail.com>
2026-09-05 16:15:57 +0800

    Set up Learning GPS Flask prototype

---
+++ app.py
@@ -0,0 +1,30 @@
+import json
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+def load_learning_map():
+    with open("data/learning_map.json", "r") as file:
+        learning_map = json.load(file)
+    return learning_map
+    
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/analyze", methods=["POST"])
+def analyze():
+    goal = request.form["goal"]
+
+    learning_story = request.form["learning_story"]
+
+    return render_template(
+        "result.html",
+        goal=goal,
+        learning_story=learning_story
+    )
+
+learning_map = load_learning_map()
+print(learning_map)
